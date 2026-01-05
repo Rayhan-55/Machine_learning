80dabd6984c8e0ab24a7f96d186d9c6d86c8db86
Rayhan-55 <asfyrayhan1106@gmail.com>
2026-01-06 00:50:37 +0600

    forth

--- data_processing.py
+++ data_processing.py
@@ -16,16 +16,31 @@ from sklearn.impute import SimpleImputer
 impute=SimpleImputer(missing_values=np.nan,strategy='mean')
 impute.fit(x[:,1:3])
 x[:,1:3]=impute.transform(x[:,1:3])#transform update kore return kore
+
 #Encoding catagorical data
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
 x=np.array(ct.fit_transform(x))
-print(x)
+#print(x)
 
 #encoding independent variables
 
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
 y=le.fit_transform(y)
-print(y)
+#print(y)
+
+#splitting the dataset into training set and test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
+#print(x_train,y_train)
+#print(x_test,y_test)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train[:,3:]=sc.fit_transform(x_train[:,3:])
+x_test[:,3:]=sc.transform(x_test[:,3:])
+print(x_train)
+print(x_test)
