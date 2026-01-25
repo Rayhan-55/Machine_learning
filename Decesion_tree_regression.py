8ace4d5e99990e06fdc5442635ca0a6a649ab3e7
Rayhan-55 <asfyrayhan1106@gmail.com>
2026-01-25 23:22:53 +0600

    forest

--- Decesion_tree_regression.py
+++ Decesion_tree_regression.py
@@ -1,27 +1,29 @@
 #Importing the Libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 #Importing the dataset
+from typing import ValuesView
 dataset=pd.read_csv('Position_Salaries.csv')
 x=dataset.iloc[:,1:-1].values
 y=dataset.iloc[:,-1].values
+#Training the Decision Tree Regression Model
 
-#Training the Random Forest  Regression Model
-from sklearn.ensemble import RandomForestRegressor
-regressor=RandomForestRegressor(n_estimators=10,random_state=0) #n_estimators means that how many tress i want  to build
+from sklearn.tree import DecisionTreeRegressor
+regressor=DecisionTreeRegressor(random_state=0)
 regressor.fit(x,y)
-
 #Predicting a new result
+
 regressor.predict([[6.5]])
 
-#Visualising the Random Forest   Regression Tree results(higher resolution)
+#Visualising the Decision Tree Regression results(higher resolution)
+
 X_grid=np.arange(min(x),max(x),0.01)
-X_grid=X_grid.reshape((len(X_grid),1))
+X_grid=X_grid.reshape(len(X_grid),1)
 plt.scatter(x,y,color='red')
 plt.plot(X_grid,regressor.predict(X_grid),color='blue')
-plt.title('Truth or Bluff(Random Forest Regression)')
+plt.title('Truth or Bluff(Decision Tree Regression)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
